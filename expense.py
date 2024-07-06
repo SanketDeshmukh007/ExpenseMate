f6f9e9e5dcfea4d8e34b384ee411eb5006fb3a89
Sanket Deshmukh <137975053+SanketDeshmukh007@users.noreply.github.com>
2024-07-06 16:35:45 +0530

    Delete expense.py

--- expense.py
+++
@@ -1,9 +0,0 @@
-class Expense:
-
-    def __init__(self, name, category, amount) -> None:
-        self.name = name
-        self.category = category
-        self.amount = amount
-
-    def __repr__(self) -> str:
-        return f"<Expense: {self.name}, {self.category}, ₹{self.amount}>"
